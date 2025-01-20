c8c91f349c01940ef77d08aca47cb388e7e7f79f
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-01-20 22:58:05 +0100

    init audienceprovider in onEnable

--- implementation/bukkit/src/main/java/eu/darkcube/system/impl/bukkit/DarkCubeSystemBukkit.java
+++ implementation/bukkit/src/main/java/eu/darkcube/system/impl/bukkit/DarkCubeSystemBukkit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024. [DarkCube]
+ * Copyright (c) 2023-2025. [DarkCube]
  * All rights reserved.
  * You may not use or redistribute this software or any associated files without permission.
  * The above copyright notice shall be included in all copies of this software.
@@ -57,6 +57,7 @@ public class DarkCubeSystemBukkit extends DarkCubePlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
         linkManager.enableLinks();
         CommandAPI.instance().register(new DarkCubeSystemCommand());
+        AdventureSupport.adventureSupport().audienceProvider();
     }
 
     @EventHandler
